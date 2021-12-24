0f087ad74f2ef24fe9afd561d544528fdb7d3b89
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-12-25 03:18:27 +0530

    del search-functionality

--- src/Pages/search/Search.js
+++ src/Pages/search/Search.js
@@ -1,31 +1,26 @@
 import { useLocation } from 'react-router-dom'
 import { useEffect,useState} from 'react'
-import useFetch from '../../Hooks/useFetch'
 import RecipeList from '../../Components/RecipeList'
 import { useTheme } from '../../Hooks/useTheme'
 import './Search.css'
 
 export default function Search() {
-    const searchedWord=new URLSearchParams(useLocation().search).get('q')
-    const url='http://localhost:3000/recipes?q='+searchedWord
-    const {data:recipes,isPending,failure:error}=useFetch(url)
-    const [filtered,setFiltered]=useState(null)
+    let [recipes,setRecipes]=useState(null)
+    let [isPending,setIsPending]=useState(false)
+    let [error,setError]=useState(null)
+    let [filtered,setFiltered]=useState(null)
+
     const {mode}=useTheme()
 
-    useEffect(()=>{
-        if(recipes){
-            setFiltered(recipes.filter(each=>(each.title.split(' ').includes(searchedWord))))
-        }
-    },[recipes])
-
+    const searchedWord=new URLSearchParams(useLocation().search).get('q')
     return(
         <div>
-            {isPending?<div>:Loading</div>:null}
-            {error?error:null}
+            {isPending?<h3 className='loading'>Loading...</h3>:null}
+            {error?<p>{error}</p>:null}
             {filtered?
             <div>
                 <h2 className={`search-results ${mode?"dark":null}`}>Recipes with " {searchedWord} "</h2>
                 <RecipeList recipes={filtered}/>
-            </div>:null}
+            </div>:<div className='search-results'>No Recipes with " {searchedWord} "</div>}
         </div>)
 }
