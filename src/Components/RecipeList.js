7a9face1be2e05b795e779d8d9c7ebc6cff205b0
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-12-25 03:17:52 +0530

    add delete-feat

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,13 +1,20 @@
 import {NavLink} from 'react-router-dom'
 import { useTheme } from '../Hooks/useTheme'
+import { fromFirebase } from '../Firebase/config'
+import deleteIcon from '../Assets/delete.svg'
+
 import './RecipeList.css'
 
 export default function RecipeList({recipes}) {
-    const {color,mode}=useTheme()
+    const {mode}=useTheme()
+    const deleteRecipe=(id)=>{
+        fromFirebase.collection('recipes').doc(id).delete()
+    }
     return (
         <div className={`recipe-list ${mode?"dark":null}`}>
             {recipes.map(eachRecipe=>(
                 <div key={eachRecipe.id} className="recipe-card">
+                    <img src={deleteIcon} alt="delete-recipe" className='delete-recipe' onClick={()=>deleteRecipe(eachRecipe.id)}/>
                     <h3>Title : {eachRecipe.title}</h3>
                     <h4>Cooking Time: {eachRecipe.cookingTime}</h4>
                     <div>Instructions: {eachRecipe.method.substring(0,100)+"..."}</div>
